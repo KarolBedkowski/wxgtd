8ee57bbe2715b17e603f0782aa982828f6ba629e
Karol Będkowski <karol.bedkowski@gmail.com>
2013-03-17 19:30:02 +0100

    gui.frame_main: update

--- wxgtd/gui/frame_main.py
+++ wxgtd/gui/frame_main.py
@@ -7,110 +7,106 @@ __author__ = "Karol Będkowski"
 __copyright__ = "Copyright (c) Karol Będkowski, 2010"
 __version__ = "2011-03-29"
 
-import locale
+import sys
+import gettext
+import time
 
 import wx
 from wx import xrc
-try:
-	from wx.lib.pubsub import pub as Publisher
-except ImportError:
-	from wx.lib.pubsub import Publisher
+#import wx.lib.customtreectrl as CT
 
 from wxgtd.lib import wxresources
 from wxgtd.lib.appconfig import AppConfig
 from wxgtd.lib import iconprovider
 #from wxgtd.lib import wxutils
 
-#from . import dlg_about
+from wxgtd.model import objects as OBJ
+from wxgtd.gui import dlg_about
+from wxgtd.gui._filtertreectrl import FilterTreeCtrl
 #from . import message_boxes as mbox
+
+_ = gettext.gettext
 
 
 class FrameMain:
 	''' Klasa głównego okna programu'''
-	def __init__(self, database):
+	def __init__(self):
 		self.res = wxresources.load_xrc_resource('wxgtd.xrc')
 		self._load_controls()
 		self._create_toolbar()
 		self._create_bindings()
-		self._setup(database)
+		self._setup_wnd()
+		self._setup()
 
 	def __getitem__(self, key):
 		ctrl = xrc.XRCCTRL(self.wnd, key)
 		if ctrl is None:
 			ctrl = self.wnd.GetMenuBar().FindItemById(xrc.XRCID(key))
-		assert ctrl is not None
+		assert ctrl is not None, 'Control %r not found' % key
 		return ctrl
 
-	@property
-	def _selected_item(self):
-		itemid = -1
-		while True:
-			itemid = self._lc_trips.GetNextItem(itemid, wx.LIST_NEXT_ALL,
-					wx.LIST_STATE_SELECTED)
-			if itemid == -1:
-				break
-			yield self._lc_trips.GetItemData(itemid)
+	def _setup(self):
+		items_list = self._items_list_ctrl
+		items_list.InsertColumn(0, _('Title'), width=400)
+		items_list.InsertColumn(1, _('Context'), width=100)
+		items_list.InsertColumn(2, _('Status'), width=100)
+		items_list.InsertColumn(3, _('Duo'), width=150)
+		self._filter_tree_ctrl.RefreshItems()
+		wx.CallAfter(self._refresh_list)
+		wx.CallAfter(self._filter_tree_ctrl.ExpandAll)
 
-	@property
-	def _has_selected_items(self):
-		return self._lc_trips.GetSelectedItemCount() > 0
-
-	@property
-	def selected_tags(self):
-		cbl = self._clb_tags
-		if wx.Platform == '__WXMSW__':
-			checked = [self._tagslist[num] for num in xrange(cbl.GetCount())
-					if cbl.IsChecked(num)]
-		else:
-			checked = [cbl.GetClientData(num) for num in xrange(cbl.GetCount())
-					if cbl.IsChecked(num)]
-		return checked
-
-	def _setup(self, database):
-		self._db = database
-		self._chart_ready = False
-		self._current_items = []
-		self._current_sorting_col = 2
-		self._tagslist = []
+	def _setup_wnd(self):
 		self.wnd.SetIcon(iconprovider.get_icon('wxgtd'))
 
 		if wx.Platform == '__WXMSW__':
+			# fix controls background
 			bgcolor = wx.SystemSettings.GetColour(wx.SYS_COLOUR_ACTIVEBORDER)
 			self.wnd.SetBackgroundColour(bgcolor)
-
-			def update_color(wnd):
-				for child in wnd.GetChildren():
-					if isinstance(child, wx.Panel):
-						child.SetBackgroundColour(bgcolor)
-					update_color(child)
-			update_color(self.wnd)
+			_update_color(self.wnd, bgcolor)
 
 		self._set_size_pos()
 
-		appconfig = AppConfig()
-		center = appconfig.get('frame_main', 'map_center')
-		zoom = appconfig.get('frame_main', 'map_zoom')
-
 	def _load_controls(self):
 		self.wnd = self.res.LoadFrame(None, 'frame_main')
-		assert self.wnd is not None
+		assert self.wnd is not None, 'Frame not found'
+		self._items_list_ctrl = self['lc_main_list']
+		# filter tree ctrl
+		filter_tree_panel = self['filter_tree_panel']
+		box = wx.BoxSizer(wx.HORIZONTAL)
+		self._filter_tree_ctrl = FilterTreeCtrl(filter_tree_panel, -1)
+		box.Add(self._filter_tree_ctrl, 1, wx.EXPAND)
+		filter_tree_panel.SetSizer(box)
 
 	def _create_bindings(self):
 		wnd = self.wnd
 		wnd.Bind(wx.EVT_CLOSE, self._on_close)
 
+		def _create_menu_bind(menu_id, handler):
+			self.wnd.Bind(wx.EVT_MENU, handler, id=xrc.XRCID(menu_id))
+
+		_create_menu_bind('menu_file_load', self._on_menu_file_load)
+		_create_menu_bind('menu_file_exit', self._on_menu_file_exit)
+		_create_menu_bind('menu_about', self._on_menu_help_about)
+
+		self._filter_tree_ctrl.Bind(wx.EVT_TREE_ITEM_ACTIVATED,
+				self._on_filter_tree_item_activated)
+#		self._filter_tree_ctrl.Bind(CT.EVT_TREE_ITEM_CHECKED,
+#				self._on_filter_tree_item_selected)
+
+	def _create_toolbar(self):
+		toolbar = self.wnd.CreateToolBar()
+		toolbar.Realize()
+
 	def _set_size_pos(self):
 		appconfig = AppConfig()
-		size = appconfig.get('frame_main', 'size')
+		size = appconfig.get('frame_main', 'size', (800, 600))
 		if size:
 			self.wnd.SetSize(size)
 		position = appconfig.get('frame_main', 'position')
 		if position:
 			self.wnd.Move(position)
 
-	def _create_toolbar(self):
-		toolbar = self.wnd.CreateToolBar()
-		toolbar.Realize()
+	# events
 
 	def _on_close(self, _event):
 		appconfig = AppConfig()
@@ -118,17 +114,49 @@ class FrameMain:
 		appconfig.set('frame_main', 'position', self.wnd.GetPositionTuple())
 		self.wnd.Destroy()
 
-	def _on_date_range_change(self, evt):
-		self._update_trips_list()
+	def _on_menu_file_load(self, _evt):
+		pass
+
+	def _on_menu_file_exit(self, _evt):
+		self.wnd.Close()
+
+	def _on_menu_help_about(self, _evt):
+		""" Show about dialog """
+		dlg_about.show_about_box(self.wnd)
+
+	def _on_filter_tree_item_activated(self, _evt):
+		self._refresh_list()
+
+	def _on_filter_tree_item_selected(self, evt):
 		evt.Skip()
 
-	def _on_sport_change(self, evt):
-		self._update_trips_list()
-		evt.Skip()
+	# logic
+
+	def _refresh_list(self):
+		tmodel = self._filter_tree_ctrl.model
+		contexts = list(tmodel.checked_items_by_parent("CONTEXTS"))
+		folders = list(tmodel.checked_items_by_parent("FOLDERS"))
+		goals = list(tmodel.checked_items_by_parent("GOALS"))
+		statuses = list(tmodel.checked_items_by_parent("STATUSES"))
+		tasks = OBJ.Task.select_by_filters(contexts, folders, goals, statuses)
+		items_list = self._items_list_ctrl
+		items_list.DeleteAllItems()
+		for task in tasks:
+			idx = items_list.InsertStringItem(sys.maxint, task.title)
+			items_list.SetStringItem(idx, 1, task.context.title if task.context
+					else '')
+			items_list.SetStringItem(idx, 2, task.status_name)
+			items_list.SetStringItem(idx, 3, format_date(task.due_date))
 
 
-def format_float(value):
-	"""docstring for format_float"""
-	if value is not None:
-		return locale.format('%0.2f', value)
-	return ''
+def _update_color(wnd, bgcolor):
+	for child in wnd.GetChildren():
+		if isinstance(child, wx.Panel):
+			child.SetBackgroundColour(bgcolor)
+		_update_color(child, bgcolor)
+
+
+def format_date(timestamp):
+	if not timestamp:
+		return ''
+	return time.strftime('%x %X', time.localtime(timestamp))
