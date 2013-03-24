7220b8bda88fdd8b8cb1de19462c7bc46f13e046
Karol Będkowski <karol.bedkowski@gmail.com>
2013-03-24 16:51:13 +0100

    gui.DlgBase: obsługa tworzenia pojedynczych okien dla danych obiektów

--- wxgtd/gui/_base_dialog.py
+++ wxgtd/gui/_base_dialog.py
@@ -8,6 +8,7 @@ __author__ = "Karol Będkowski"
 __copyright__ = "Copyright (c) Karol Będkowski, 2010-2013"
 __version__ = "2010-11-25"
 
+import weakref
 
 import wx
 from wx import xrc
@@ -18,9 +19,12 @@ from wxgtd.lib import iconprovider
 
 
 class BaseDialog:
+	_windows = weakref.WeakValueDictionary()
+
 	def __init__(self, parent, name='dialog', resource='wxgtd.xrc', icon='wxgtd',
 			save_pos=True):
 		self._name = name
+		self._obj_key = None
 		self._save_pos = save_pos
 		res = wxresources.load_xrc_resource(resource)
 		assert res is not None, 'resource %s not found' % resource
@@ -31,10 +35,27 @@ class BaseDialog:
 		self._create_bindings()
 		self._setup_wnd(icon, save_pos)
 
-	def run(self):
-		res = self._wnd.ShowModal() in (wx.ID_OK, wx.ID_SAVE)
-		self._wnd.Destroy()
-		return res
+	@classmethod
+	def create(cls, key, *args, **kwargs):
+		""" Utworzenie okna jeżeli należy zachować pojedyńcze okno dla obiektu
+			@param key - identyfikator obiektu dla którego tworzone jest okno
+		"""
+		if not key:
+			return cls(*args, **kwargs)
+		dlg = cls._windows.get(key)
+		if dlg:
+			wx.CallAfter(dlg._wnd.Raise)
+		else:
+			cls._windows[key] = dlg = cls(*args, **kwargs)
+			dlg._obj_key = key
+		return dlg
+
+	def run(self, modal=False):
+		if modal:
+			res = self._wnd.ShowModal() in (wx.ID_OK, wx.ID_SAVE)
+			self._wnd.Destroy()
+			return res
+		self._wnd.Show()
 
 	def __getitem__(self, key):
 		if isinstance(key, (str, unicode)):
@@ -64,19 +85,30 @@ class BaseDialog:
 
 	def _create_bindings(self):
 		self._wnd.Bind(wx.EVT_CLOSE, self._on_close)
-		self._wnd.Bind(wx.EVT_BUTTON, self._on_close, id=wx.ID_CLOSE)
+		self._wnd.Bind(wx.EVT_BUTTON, self._on_cancel, id=wx.ID_CLOSE)
 		self._wnd.Bind(wx.EVT_BUTTON, self._on_save, id=wx.ID_SAVE)
 		self._wnd.Bind(wx.EVT_BUTTON, self._on_ok, id=wx.ID_OK)
-		self._wnd.Bind(wx.EVT_BUTTON, self._on_close, id=wx.ID_CANCEL)
+		self._wnd.Bind(wx.EVT_BUTTON, self._on_cancel, id=wx.ID_CANCEL)
 
-	def _on_close(self, _event):
+	def _on_close(self, evt):
 		if self._save_pos:
 			self._appconfig.set(self._name, 'size', self._wnd.GetSizeTuple())
 			self._appconfig.set(self._name, 'position', self._wnd.GetPositionTuple())
-		self._wnd.EndModal(wx.ID_CLOSE)
+		if self._obj_key and self._obj_key in self._windows:
+			del self._windows[self._obj_key]
+		self._wnd.Destroy()
+
+	def _on_cancel(self, _evt):
+		if self._wnd.IsModal():
+			self._wnd.EndModal(wx.ID_CLOSE)
+		else:
+			self._wnd.Close()
 
 	def _on_ok(self, _evt):
-		self._wnd.EndModal(wx.ID_OK)
+		if self._wnd.IsModal():
+			self._wnd.EndModal(wx.ID_OK)
+		else:
+			self._wnd.Close()
 
 	def _on_save(self, evt):
 		self._on_ok(evt)
