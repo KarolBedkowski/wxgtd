738e7d2a726636349216eb278ad8057963412765
Karol Będkowski <karol.bedkowski@gmail.com>
2013-04-22 20:54:20 +0200

    sync: + tworzenie backupów przed synchronizacją

--- wxgtd/model/sync.py
+++ wxgtd/model/sync.py
@@ -5,14 +5,19 @@
 
 import logging
 import gettext
+import os
+import datetime
 
 try:
 	from wx.lib.pubsub.pub import Publisher
 except ImportError:
 	from wx.lib.pubsub import Publisher
 
+from wxgtd.lib import appconfig
+
 import exporter
 import loader
+
 
 _LOG = logging.getLogger(__name__)
 _ = gettext.gettext
@@ -41,6 +46,8 @@ def _notify_progress(progress, msg):
 def sync(filename):
 	""" Synchronizacja danych z podanym plikiem """
 	_LOG.info("sync: %r", filename)
+	_notify_progress(0, _("Creating backup"))
+	create_backup()
 	_notify_progress(1, _("Checking sync lock"))
 	if exporter.create_sync_lock(filename):
 		try:
@@ -55,3 +62,49 @@ def sync(filename):
 			_notify_progress(99, _("Removing sync lock"))
 			exporter.delete_sync_lock(filename)
 		_notify_progress(100, _("Completed"))
+
+
+def create_backup():
+	""" Create backup current data in database.
+
+	Format of backup file is identical with synchronization file.
+	Backup are stored for default in ~/.local/share/wxgtd/backups/
+
+	Configuration in wxgtd.conf:
+	[backup]
+	number_copies = 21
+	location = <path to dir>
+	"""
+	appcfg = appconfig.AppConfig()
+	backup_dir = appcfg.get('backup', 'location')
+	if backup_dir:
+		backup_dir = os.path.expanduser(backup_dir)
+	else:
+		backup_dir = os.path.join(appcfg.user_share_dir, 'backups')
+	filename = os.path.join(backup_dir,
+			"BACKUP_" + datetime.date.today().isoformat() + ".json.zip")
+	_LOG.info('create_backup: %s', filename)
+	if os.path.isfile(filename):
+		_LOG.info("create_backup: today backup already exists; skipping...")
+		return True
+	if os.path.isdir(backup_dir):
+		num_files_to_keep = int(appcfg.get('backup', 'number_copies', 21))
+		# backup dir exists; check number of files and delete if more than 21
+		files = sorted((fname for fname in os.listdir(backup_dir)
+				if fname.startswith('BACKUP') and fname.endswith('.json.zip')),
+				reverse=True)
+		if len(files) >= num_files_to_keep:
+			for fname in files[num_files_to_keep:]:
+				_LOG.info('create_backup: delete backup: %r', fname)
+				os.unlink(os.path.join(backup_dir, fname))
+	else:
+		try:
+			os.mkdir(backup_dir)
+		except IOError as error:
+			_LOG.error('create_backup: create dir error: %s', str(error))
+			return False
+	# backup is regular export (zip)
+	exporter.save_to_file(os.path.join(backup_dir, filename),
+			internal_fname="GDT_SYNC.json")
+	_LOG.info('create_backup: COMPLETED %s', filename)
+	return True
