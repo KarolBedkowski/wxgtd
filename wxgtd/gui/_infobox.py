0383a67e3b95026f3c21be02b4861eddb4df2aa7
Karol Będkowski <karol.bedkowski@gmail.com>
2013-05-05 21:53:10 +0200

    gui: renderowanie informacji na panelu parenta

--- wxgtd/gui/_infobox.py
+++ wxgtd/gui/_infobox.py
@@ -87,7 +87,7 @@ _TASK_TYPE_ICONS = {enums.TYPE_TASK: "",
 
 
 def draw_icons(mdc, task, overdue, active_only):
-	font = wx.Font(10, wx.NORMAL, wx.NORMAL, wx.NORMAL, False)
+	font = wx.Font(8, wx.NORMAL, wx.NORMAL, wx.NORMAL, False)
 	mdc.SetFont(font)
 	inf_y_offset = mdc.GetTextExtent("Agw")[1] + 10
 	if task.starred:
@@ -96,19 +96,56 @@ def draw_icons(mdc, task, overdue, active_only):
 				task.child_count
 	icon = _TASK_TYPE_ICONS.get(task.type)
 	if icon:
-		mdc.DrawBitmap(iconprovider.get_image(icon), 18, 7, False)
+		mdc.DrawBitmap(iconprovider.get_image(icon), 16, 7, False)
 	child_count = task.active_child_count if active_only else \
 			task.child_count
 	if child_count > 0:
 		info = ""
 		overdue = task.child_overdue
 		if overdue > 0:
-			info += " %d / " % overdue
-		info += " %d " % child_count
-		mdc.DrawText(info, 36, 7)
+			info += "%d / " % overdue
+		info += "%d" % child_count
+		mdc.DrawText(info, 32, 7)
 	if task.alarm:
 		mdc.DrawBitmap(iconprovider.get_image('alarm_small'), 0, inf_y_offset,
 				False)
 	if task.repeat_pattern and task.repeat_pattern != 'Norepeat':
-		mdc.DrawBitmap(iconprovider.get_image('repeat_small'), 18, inf_y_offset,
+		mdc.DrawBitmap(iconprovider.get_image('repeat_small'), 16, inf_y_offset,
 				False)
+
+
+class TaskInfoPanel(wx.Panel):
+	def __init__(self, *args, **kwargs):
+		wx.Panel.__init__(self, *args, **kwargs)
+		self.task = None
+		self.overdue = False
+		self.Bind(wx.EVT_PAINT, self._on_paint)
+
+	def _on_paint(self, _evt):
+		dc = wx.BufferedPaintDC(self)
+		self.PrepareDC(dc)
+		bg = wx.Brush(self.GetBackgroundColour())
+		dc.SetBackground(bg)
+		dc.Clear()
+		if self.task:
+			draw_info(dc, self.task, self.overdue)
+		dc.EndDrawing()
+
+
+class TaskIconsPanel(wx.Panel):
+	def __init__(self, *args, **kwargs):
+		wx.Panel.__init__(self, *args, **kwargs)
+		self.task = None
+		self.overdue = False
+		self.active_only = False
+		self.Bind(wx.EVT_PAINT, self._on_paint)
+
+	def _on_paint(self, _evt):
+		dc = wx.BufferedPaintDC(self)
+		self.PrepareDC(dc)
+		bg = wx.Brush(self.GetBackgroundColour())
+		dc.SetBackground(bg)
+		dc.Clear()
+		if self.task:
+			draw_icons(dc, self.task, self.overdue, self.active_only)
+		dc.EndDrawing()
