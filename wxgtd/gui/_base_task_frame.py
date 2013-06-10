2b295d70293671fe1b1e56a52c5e83b95938b0e3
Karol Będkowski <karol.bedkowski@gmail.com>
2013-06-10 20:16:12 +0200

    dev: obejście walidacji przy zmianie typu zadań

--- wxgtd/gui/_base_task_frame.py
+++ wxgtd/gui/_base_task_frame.py
@@ -219,7 +219,14 @@ class BaseTaskFrame(BaseFrame):
 	@wxutils.call_after
 	def _on_task_type_change(self):
 		""" Called after task type change. """
+		fake_title = False
+		if not self['tc_title'].GetValue().strip():
+			self['tc_title'].SetValue("<?>")
+			fake_title = True
 		self._transfer_data_from_window()
+		if fake_title:
+			self._task.title = ""
+			self['tc_title'].SetValue("")
 		self._controller.change_task_type()
 
 	@wxutils.call_after
